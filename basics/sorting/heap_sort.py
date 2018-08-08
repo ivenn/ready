477caee8b5459e80211323e8435fdd8bcf42dad9
ivenn <abokov.nnov@gmail.com>
2018-08-08 12:35:11 -0700

    correcting misprint in heap_sort

--- basics/sorting/heap_sort.py
+++ basics/sorting/heap_sort.py
@@ -21,7 +21,7 @@ def heap_sort(arr, n):
 
     for i in range(n - 1, -1, -1):
         arr[i], arr[0] = arr[0], arr[i]
-        min_heapify(arr, i, 0)
+        max_heapify(arr, i, 0)
 
 
 def main():
